df5ca54897201c84563a11ab8f8bf265d5f43ac0
MikkelRosenbek <miro0002@stud.kea.dk>
2025-09-11 13:04:44 +0200

    Repo updated

--- src/main/java/dk/ek/libary/catalog/repository/WorkRepository.java
+++ src/main/java/dk/ek/libary/catalog/repository/WorkRepository.java
@@ -2,7 +2,6 @@ package dk.ek.libary.catalog.repository;
 
 import dk.ek.libary.catalog.model.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface WorkRepository extends JpaRepository <Work, Long> {
