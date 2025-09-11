7b91c1bacafb8f65c02f0e0848177ed4bf26819e
MikkelRosenbek <miro0002@stud.kea.dk>
2025-09-11 14:03:30 +0200

    InitData updated

--- src/main/java/dk/ek/libary/common/InitData.java
+++ src/main/java/dk/ek/libary/common/InitData.java
@@ -1,16 +1,10 @@
 package dk.ek.libary.common;
 
-import dk.ek.libary.catalog.model.Edition;
-import dk.ek.libary.catalog.model.Publisher;
-import dk.ek.libary.catalog.model.Work;
-import dk.ek.libary.catalog.model.WorkType;
-import dk.ek.libary.catalog.repository.EditionRepository;
-import dk.ek.libary.catalog.repository.PublisherRepository;
-import dk.ek.libary.catalog.repository.WorkRepository;
+import dk.ek.libary.catalog.model.*;
+import dk.ek.libary.catalog.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -19,7 +13,9 @@ public class InitData implements CommandLineRunner {
     private final EditionRepository editionRepository;
     private final PublisherRepository publisherRepository;
 
-    public InitData(WorkRepository workRepository, EditionRepository editionRepository, PublisherRepository publisherRepository) {
+    public InitData(WorkRepository workRepository,
+                    EditionRepository editionRepository,
+                    PublisherRepository publisherRepository) {
         this.workRepository = workRepository;
         this.editionRepository = editionRepository;
         this.publisherRepository = publisherRepository;
@@ -28,12 +24,45 @@ public class InitData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // --- Works ---
-        Work work1 = new Work(null, "The Great Gatsby", WorkType.NOVEL, "A classic novel set in the Jazz Age", "F. Scott Fitzgerald", "American literature, 1920s");
-        Work work2 = new Work(null, "To Kill a Mockingbird", WorkType.NOVEL, "A novel about racial injustice and childhood", "Harper Lee", "Civil rights, American South");
-        Work work3 = new Work(null, "The Odyssey", WorkType.EPIC, "An ancient Greek epic poem", "Homer", "Mythology, Adventure");
-        Work work4 = new Work(null, "A Brief History of Time", WorkType.NONFICTION, "A book on cosmology", "Stephen Hawking", "Science, Universe");
-        Work work5 = new Work(null, "Hamlet", WorkType.PLAY, "A Shakespearean tragedy", "William Shakespeare", "Drama, Revenge");
+        Work work1 = new Work(null, "The Great Gatsby", WorkType.NOVEL, "A classic novel set in the Jazz Age");
+        Author fitzgerald = new Author(null, "F. Scott Fitzgerald");
+        Subject americanLit = new Subject(null, "American Literature");
+        Subject twenties = new Subject(null, "1920s");
+        work1.addAuthor(fitzgerald);
+        work1.addSubject(americanLit);
+        work1.addSubject(twenties);
+
+        Work work2 = new Work(null, "To Kill a Mockingbird", WorkType.NOVEL, "A novel about racial injustice and childhood");
+        Author lee = new Author(null, "Harper Lee");
+        Subject civilRights = new Subject(null, "Civil Rights");
+        Subject south = new Subject(null, "American South");
+        work2.addAuthor(lee);
+        work2.addSubject(civilRights);
+        work2.addSubject(south);
+
+        Work work3 = new Work(null, "The Odyssey", WorkType.EPIC, "An ancient Greek epic poem");
+        Author homer = new Author(null, "Homer");
+        Subject mythology = new Subject(null, "Mythology");
+        Subject adventure = new Subject(null, "Adventure");
+        work3.addAuthor(homer);
+        work3.addSubject(mythology);
+        work3.addSubject(adventure);
+
+        Work work4 = new Work(null, "A Brief History of Time", WorkType.NONFICTION, "A book on cosmology");
+        Author hawking = new Author(null, "Stephen Hawking");
+        Subject science = new Subject(null, "Science");
+        Subject universe = new Subject(null, "Universe");
+        work4.addAuthor(hawking);
+        work4.addSubject(science);
+        work4.addSubject(universe);
+
+        Work work5 = new Work(null, "Hamlet", WorkType.PLAY, "A Shakespearean tragedy");
+        Author shakespeare = new Author(null, "William Shakespeare");
+        Subject drama = new Subject(null, "Drama");
+        Subject revenge = new Subject(null, "Revenge");
+        work5.addAuthor(shakespeare);
+        work5.addSubject(drama);
+        work5.addSubject(revenge);
 
         workRepository.saveAll(List.of(work1, work2, work3, work4, work5));
 
@@ -44,11 +73,7 @@ public class InitData implements CommandLineRunner {
         Publisher publisher4 = new Publisher(null, "Bantam Books", "New York, NY", "info@bantambooks.com");
         Publisher publisher5 = new Publisher(null, "Methuen & Co.", "London, UK", "info@methuen.co.uk");
 
-        publisher1 = publisherRepository.save(publisher1);
-        publisher2 = publisherRepository.save(publisher2);
-        publisher3 = publisherRepository.save(publisher3);
-        publisher4 = publisherRepository.save(publisher4);
-        publisher5 = publisherRepository.save(publisher5);
+        publisherRepository.saveAll(List.of(publisher1, publisher2, publisher3, publisher4, publisher5));
 
         // --- Editions ---
         Edition edition1 = new Edition(null, "1st Edition", 1925, "Hardcover");
